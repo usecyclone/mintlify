c8e067527fd9aec98ce2b3ba51f8bc07746f745e
Tony Yang <tonyyanga@gmail.com>
2023-06-04 19:10:07 -0700

    add CLI integration

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import { cli } from './cli.js';
+import NodeClient from 'cyclone-node';
 export { cli };
+var cyclone = new NodeClient("mintlify", "phc_Belh475IYfPoF9bke7r9ReO3m7WIa21C5ftRvD10Pvs");
 cli();
